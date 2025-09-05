401698090a727aa3186a453d7831805fd6be9336
HeikonSilva <heikon.costa@etec.sp.gov.br>
2025-09-05 15:57:59 -0300

    add router functionality and update index.html structure

---
+++ src/router.js
@@ -0,0 +1,13 @@
+const nav = `<a href="/#/">Home</a> | 
+                   <a href="/#/about">About</a> | 
+                   <a href="/#/contact">Contact</a>`;
+const routes = {
+  "": `<h1>Home</h1>${nav}<p>Welcome home!</p>`,
+  about: `<h1>About</h1>${nav}<p>This is a tiny SPA</p>`,
+};
+const render = (path) => {
+  document.querySelector("#app").innerHTML =
+    routes[path.replace(/^#\//, "")] ?? `<h1>404</h1>${nav}`;
+};
+window.onhashchange = (evt) => render(window.location.hash);
+render(window.location.hash);
