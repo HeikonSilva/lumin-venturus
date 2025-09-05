e939d0f70a874d9f489f42477ff12cc96a084640
HeikonSilva <heikon.costa@etec.sp.gov.br>
2025-09-05 16:01:28 -0300

    fixed navbar href

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
-const nav = `<a href="/#/">Home</a> | 
-                   <a href="/#/about">About</a> | 
-                   <a href="/#/contact">Contact</a>`;
+const nav = `<a href="/src/#/">Home</a> | 
+                   <a href="/src/#/about">About</a> | 
+                   <a href="/src/#/contact">Contact</a>`;
+
 const routes = {
   "": `<h1>Home</h1>${nav}<p>Welcome home!</p>`,
   about: `<h1>About</h1>${nav}<p>This is a tiny SPA</p>`,
