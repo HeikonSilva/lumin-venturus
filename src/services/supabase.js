a85b7746c92d70da90c8ec094fb0488f7df6ad04
HeikonSilva <heikon.costa@etec.sp.gov.br>
2025-11-11 19:42:57 -0300

    small fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL || 'https://uatfviycqqinxhjrzfje.supabase.co',
-  import.meta.env.VITE_SUPABASE_KEY || 'sb_publishable_mUZ01PQE_oI137pavuSwRA_2qyO0mUI'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
